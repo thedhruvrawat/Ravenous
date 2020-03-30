2fa993087a34c65b39b0718dd3e4bc046bdab3ca
Dhruv Rawat <dhruv.noida175@gmail.com>
2020-03-30 21:39:54 +0530

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import BusinessList from '../Business/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>ravenous</h1>
+      {/* <!-- Add SearchBar component here -->
+      <!-- Add BusinessList component here -->  */}
+    </div>
+  );
+}
+
+export default App;
